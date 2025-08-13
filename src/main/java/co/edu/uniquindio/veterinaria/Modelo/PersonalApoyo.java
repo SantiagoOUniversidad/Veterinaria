0d1a39c97ddbc43617196537b197080e756d85f5
santiagooff <santiago.olarteb@uqvirtual.edu.co>
2025-08-12 21:43:47 -0500

    Crud propietario en personal apoyo

--- src/main/java/co/edu/uniquindio/veterinaria/Modelo/PersonalApoyo.java
+++ src/main/java/co/edu/uniquindio/veterinaria/Modelo/PersonalApoyo.java
@@ -11,4 +11,33 @@ import java.util.List;
 @ToString(callSuper = true)
 
 public class PersonalApoyo extends Persona {
+
+    public boolean agregarPropietario(Veterinaria veterinaria, Propietario propietario) {
+        if (veterinaria == null || propietario == null) {
+            return false;
+        }
+        return veterinaria.agregarPersona(propietario);
+    }
+
+    public boolean eliminarPropietario(Veterinaria veterinaria, String id) {
+        if (veterinaria == null || id == null) {
+            return false;
+        }
+        return veterinaria.eliminarPersona(id);
+    }
+
+    public boolean actualizarPropietario(Veterinaria veterinaria, Propietario propietario, String id) {
+        if (veterinaria == null || id == null || propietario == null) {
+            return false;
+        }
+        return veterinaria.actualizarPersona(id, propietario);
+    }
+
+    public Persona mostrarPropietario(Veterinaria veterinaria, String id) {
+        if (veterinaria == null || id == null) {
+            return null;
+        }
+        return veterinaria.mostrarPersona(id);
+    }
+
 }
