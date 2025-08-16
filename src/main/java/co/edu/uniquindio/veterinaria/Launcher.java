7669b0cc99c3c86ab4d1a223d801858916e29373
santiagooff <santiago.olarteb@uqvirtual.edu.co>
2025-08-15 21:29:04 -0500

    Proyecto finalizado

--- src/main/java/co/edu/uniquindio/veterinaria/Launcher.java
+++ src/main/java/co/edu/uniquindio/veterinaria/Launcher.java
@@ -3,6 +3,8 @@ package co.edu.uniquindio.veterinaria;
 import javafx.application.Application;
 
 public class Launcher {
+    //Finalizado
+
     public static void main(String[] args) {
         Application.launch(VeterinariaApp.class, args);
     }
